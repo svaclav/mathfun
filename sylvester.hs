5d74f6b5aea9be3ce819ac0f6eacad9ba4818941
Vaclav Strachon <vaclav.strachon@gmail.com>
2014-09-27 14:01:31 +0200

    added IO

--- sylvester.hs
+++ sylvester.hs
@@ -1,2 +1,8 @@
+module Main where
+main = do 
+    putStrLn "Let's computer Sylvester's sequence:"
+    number <- getLine
+    putStrLn ("Sylvester's seqeuence is " ++ show (sylvester (read number))  ++ ".")
+
 sylvester 0 = 2
 sylvester s = sylvester p ^ 2 - sylvester p + 1 where p = s - 1
