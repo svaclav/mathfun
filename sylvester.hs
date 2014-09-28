e1d65e3cb24ac04bb6d91442e2d86cf9a826cd3d
Vaclav Strachon <vaclav.strachon@gmail.com>
2014-09-28 15:07:12 +0200

    typo

--- sylvester.hs
+++ sylvester.hs
@@ -8,4 +8,4 @@ main = do
     putStrLn ("Sylvester's seqeuence is " ++ show (sylvester (read number))  ++ ".")
 
 sylvester 0 = 2
-sylvester s = sylvester p ^ 2 - sylvester p + 1 where p = s - 1
+sylvester s = (sylvester p ^ 2 - sylvester p) + 1 where p = s - 1
